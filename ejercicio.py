8801adcd146490bbba99cfbf56471240b16c9fa8
claudiaalozano <clau.lozano.perez@gmail.com>
2023-05-16 21:38:51 +0200

    creo la clase constructor para que no exista un bucle infinito

--- ejercicio.py
+++ ejercicio.py
@@ -8,3 +8,30 @@ out_index = 0
 mutex = threading.Semaphore()
 vacio= threading.Semaphore(capacidad)
 lleno= threading.Semaphore(0)
+
+#Clase Productor
+class Productor(threading.Thread):
+    
+    def run(self):
+        global capacidad
+        global in_index
+        global out_index
+        global buffer
+        global mutex
+        global vacio
+        global lleno
+        items_producidos = 0
+        contador=0
+
+        while True:
+            vacio.acquire()
+            mutex.acquire()
+            contador += 1
+            buffer[in_index] = contador
+            in_index = (in_index + 1) % capacidad
+            print("Productor", self.name, "produjo:", in_index)
+            
+            mutex.release()
+            lleno.release()
+            time.sleep(1)
+            items_producidos += 1
